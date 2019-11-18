9f5a6c9a1fc029e58c06c9d26bbc8a389a749cbb
anqi <anqi@youxin.com>
2019-11-18 11:31:12 +0800

    项目初始化

---
+++ jdklearn/src/main/java/com/dachui/jdklearn/JdklearnApplication.java
@@ -0,0 +1,13 @@
+package com.dachui.jdklearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdklearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdklearnApplication.class, args);
+    }
+
+}
