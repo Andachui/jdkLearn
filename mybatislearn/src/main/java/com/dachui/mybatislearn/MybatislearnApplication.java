866238f8950c886c35c6c3b9e3607789723cf293
anqi <anqi@youxin.com>
2019-11-18 19:49:37 +0800

    新项目初始化

---
+++ mybatislearn/src/main/java/com/dachui/mybatislearn/MybatislearnApplication.java
@@ -0,0 +1,13 @@
+package com.dachui.mybatislearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatislearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatislearnApplication.class, args);
+    }
+
+}
---
+++ mybatislearn/src/test/java/com/dachui/mybatislearn/MybatislearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dachui.mybatislearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatislearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
